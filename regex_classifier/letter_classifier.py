3a2e3e269ce5a544bd96b5ec2289eb629472fe9a
Sam Banning <BanningS@SMH.CA>
2017-11-08 17:25:39 -0500

    fixed some issues with report generation

--- regex_classifier/letter_classifier.py
+++ regex_classifier/letter_classifier.py
@@ -13,6 +13,12 @@ class LetterClassifier(object):
         self._regs = []
 
     def add_reg(self, reg, score):
+        """
+        Add a regular expression
+        :param reg: a regular expression object
+        :param score: a number, positive indicating a positive association, and negative indicating a negative association
+        :return: None
+        """
         self._regs.append((reg, score))
 
     def score_letter(self, letter):
@@ -23,7 +29,7 @@ class LetterClassifier(object):
         current_score = 0
         matches = []
         for reg, score in self._regs:
-            match = re.search(reg, letter)
+            match = reg.search(letter)
             if match:
                 current_score += score
                 matches.append((match, score))
@@ -32,12 +38,20 @@ class LetterClassifier(object):
     def generate_report(self, patient_ids, letters, labels, output_folder='.'):
         failures = []
         for i, letter in enumerate(letters):
-            label = labels
+            label = labels[i]
             score, matches = self._score_letter(letter)
-            if score > 0 != label[i]:
+            if (score > 0) != label:
                 failures.append({'patient_id':patient_ids[i], 'letter':letter, 'label':label, 'score':score, 'matches':matches})
 
         template = env.get_template('report.html')
+        if not os.path.isdir(output_folder):
+            os.mkdir(output_folder)
+        else:
+            # remove any previously generated reports in this folder
+            for filename in os.listdir(output_folder):
+                if re.match(r'failure[0-9]+\.html', filename):
+                    os.remove(os.path.join(output_folder, filename))
+        # generate the new report
         for i, failure in enumerate(failures):
             output = template.render(failures=failures, current_failure=failure)
             with open(os.path.join(output_folder, "failure{}.html".format(i+1)), "w") as fh:
